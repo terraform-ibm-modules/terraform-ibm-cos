b9268d3a77bbdf89b9156df7c36257a6012b1243
Daniel Butler <dannyeb@gmail.com>
2022-11-17 14:31:07 +0000

    test: add region select

--- tests/other_test.go
+++ tests/other_test.go
@@ -2,6 +2,8 @@
 package test
 
 import (
+	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/cloudinfo"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,14 +14,24 @@ const resourceGroup = "geretain-test-resources"
 const multipleBucketsExampleTerraformDir = "examples/complete-multiple-buckets"
 const bucketWithoutTrackingExampleTerraformDir = "examples/bucket-without-tracking-monitoring"
 
+// TestMain will be run before any parallel tests, used to set up a shared InfoService object to track region usage
+// for multiple tests
+func TestMain(m *testing.M) {
+	sharedInfoSvc, _ = cloudinfo.NewCloudInfoServiceFromEnv("TF_VAR_ibmcloud_api_key", cloudinfo.CloudInfoServiceOptions{})
+
+	os.Exit(m.Run())
+}
+
 func TestRunMultipleBucketsExample(t *testing.T) {
 	t.Parallel()
 
 	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
-		Testing:       t,
-		TerraformDir:  multipleBucketsExampleTerraformDir,
-		ResourceGroup: resourceGroup,
-		Prefix:        "cos-module-buckets",
+		Testing:                       t,
+		TerraformDir:                  multipleBucketsExampleTerraformDir,
+		ResourceGroup:                 resourceGroup,
+		Prefix:                        "cos-module-buckets",
+		CloudInfoService:              sharedInfoSvc,
+		ExcludeActivityTrackerRegions: true,
 	})
 
 	output, err := options.RunTestConsistency()
@@ -31,10 +43,12 @@ func TestRunBucketWithoutTrackingExample(t *testing.T) {
 	t.Parallel()
 
 	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
-		Testing:       t,
-		TerraformDir:  bucketWithoutTrackingExampleTerraformDir,
-		ResourceGroup: resourceGroup,
-		Prefix:        "cos-module-wo-tracking",
+		Testing:                       t,
+		TerraformDir:                  bucketWithoutTrackingExampleTerraformDir,
+		ResourceGroup:                 resourceGroup,
+		Prefix:                        "cos-module-wo-tracking",
+		CloudInfoService:              sharedInfoSvc,
+		ExcludeActivityTrackerRegions: true,
 	})
 
 	output, err := options.RunTestConsistency()
--- tests/pr_test.go
+++ tests/pr_test.go
@@ -2,6 +2,8 @@
 package test
 
 import (
+	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/cloudinfo"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,14 +14,26 @@ const completeExampleTerraformDir = "examples/complete"
 
 const resourceGroup = "geretain-test-resources"
 
+var sharedInfoSvc *cloudinfo.CloudInfoService
+
+// TestMain will be run before any parallel tests, used to set up a shared InfoService object to track region usage
+// for multiple tests
+func TestMain(m *testing.M) {
+	sharedInfoSvc, _ = cloudinfo.NewCloudInfoServiceFromEnv("TF_VAR_ibmcloud_api_key", cloudinfo.CloudInfoServiceOptions{})
+
+	os.Exit(m.Run())
+}
+
 func TestRunDefaultExample(t *testing.T) {
 	t.Parallel()
 
 	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
-		Testing:       t,
-		TerraformDir:  completeExampleTerraformDir,
-		ResourceGroup: resourceGroup,
-		Prefix:        "cos-module-test",
+		Testing:                       t,
+		TerraformDir:                  completeExampleTerraformDir,
+		ResourceGroup:                 resourceGroup,
+		CloudInfoService:              sharedInfoSvc,
+		ExcludeActivityTrackerRegions: true,
+		Prefix:                        "cos-module-test",
 	})
 
 	output, err := options.RunTestConsistency()
@@ -31,10 +45,12 @@ func TestRunUpgradeExample(t *testing.T) {
 	t.Parallel()
 
 	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
-		Testing:       t,
-		TerraformDir:  completeExampleTerraformDir,
-		ResourceGroup: resourceGroup,
-		Prefix:        "cos-module-upg",
+		Testing:                       t,
+		TerraformDir:                  completeExampleTerraformDir,
+		ResourceGroup:                 resourceGroup,
+		CloudInfoService:              sharedInfoSvc,
+		ExcludeActivityTrackerRegions: true,
+		Prefix:                        "cos-module-upg",
 	})
 
 	output, err := options.RunTestUpgrade()
