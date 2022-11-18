c0c479f18928fd52cac771da9cc0f3bea6258b47
Daniel Butler <dannyeb@gmail.com>
2022-11-18 10:04:36 +0000

    test: fix other_test

--- tests/other_test.go
+++ tests/other_test.go
@@ -2,25 +2,14 @@
 package test
 
 import (
-	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/cloudinfo"
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
 )
 
-const resourceGroup = "geretain-test-resources"
 const multipleBucketsExampleTerraformDir = "examples/complete-multiple-buckets"
 const bucketWithoutTrackingExampleTerraformDir = "examples/bucket-without-tracking-monitoring"
-
-// TestMain will be run before any parallel tests, used to set up a shared InfoService object to track region usage
-// for multiple tests
-func TestMain(m *testing.M) {
-	sharedInfoSvc, _ = cloudinfo.NewCloudInfoServiceFromEnv("TF_VAR_ibmcloud_api_key", cloudinfo.CloudInfoServiceOptions{})
-
-	os.Exit(m.Run())
-}
 
 func TestRunMultipleBucketsExample(t *testing.T) {
 	t.Parallel()
