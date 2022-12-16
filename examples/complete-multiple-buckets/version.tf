a6736dc67f7f7775dd37d8b9667bf3cbb33d854d
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2022-12-16 10:47:12 +0000

    Update version.tf

--- examples/complete-multiple-buckets/version.tf
+++ examples/complete-multiple-buckets/version.tf
@@ -4,11 +4,11 @@ terraform {
     # Pin to the lowest provider version of the range defined in the main module to ensure lowest version still works
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = "1.45.0"
+      version = "1.48.0"
     }
     restapi = {
       source  = "Mastercard/restapi"
-      version = "1.17.0"
+      version = "1.18.0"
     }
   }
 }
