6992976589733df5626bcd5689f862607c562e90
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2023-01-05 12:04:58 +0000

    fix(deps): update terraform ibm to v1.49.0 (#106)
    * fix(deps): update terraform ibm to v1.49.0
    
    * docs: committing files modified by the hook
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- version.tf
+++ version.tf
@@ -4,7 +4,7 @@ terraform {
     # Use "greater than or equal to" range in modules
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = ">= 1.48.0"
+      version = ">= 1.49.0"
     }
     # ignore linter error, restapi provider needed for key-protect-module/aliases when consuming key-protect-all-inclusive-module
     #tflint-ignore: terraform_unused_required_providers
