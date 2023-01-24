0bd039578e722aece94a00cfc7a6df41e3dd50f3
Daniel Butler <dannyeb@gmail.com>
2023-01-24 15:23:55 +0000

    test: add crb
    Signed-off-by: Daniel Butler <dannyeb@gmail.com>

--- fscloud/providers.tf
+++ fscloud/providers.tf
@@ -17,4 +17,5 @@ provider "restapi" {
 # ibmcloud_api_key is only required for tests, due to need to create resource group
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
+  region           = var.region
 }
