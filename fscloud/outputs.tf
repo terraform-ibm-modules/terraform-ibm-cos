d24fb7de7c9d85ff0b69016c21002e753a2db84f
Daniel Butler <dannyeb@gmail.com>
2023-01-24 15:18:05 +0000

    test: add fscloud example
    Signed-off-by: Daniel Butler <dannyeb@gmail.com>

---
+++ fscloud/outputs.tf
@@ -0,0 +1,9 @@
+output "bucket_name1" {
+  description = "Bucket Name"
+  value       = module.cos_bucket1.bucket_name
+}
+
+output "bucket_name2" {
+  description = "Bucket Name"
+  value       = module.cos_bucket2.bucket_name
+}
