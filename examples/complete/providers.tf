f1ec0ece7a087a5cedb8e0772b6e00b699b26a77
Conall O'Cofaigh <ocofaigh@ie.ibm.com>
2023-01-26 12:37:15 +0000

    test: logdna provider needed to consume new obv inst mod. SKIP UPGRADE TEST

--- examples/complete/providers.tf
+++ examples/complete/providers.tf
@@ -14,7 +14,22 @@ provider "restapi" {
   }
 }
 
-# ibmcloud_api_key is only required for tests, due to need to create resource group
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
+}
+
+locals {
+  at_endpoint = "https://api.${var.region}.logging.cloud.ibm.com"
+}
+
+provider "logdna" {
+  alias      = "at"
+  servicekey = module.observability_instances.activity_tracker_resource_key != null ? module.observability_instances.activity_tracker_resource_key : ""
+  url        = local.at_endpoint
+}
+
+provider "logdna" {
+  alias      = "ld"
+  servicekey = module.observability_instances.logdna_resource_key != null ? module.observability_instances.logdna_resource_key : ""
+  url        = local.at_endpoint
 }
