1ef7bc80a6175d7943fcdbe0b3ddb3925e24bd82
Conall O'Cofaigh <ocofaigh@ie.ibm.com>
2023-02-07 10:28:05 +0000

    test: go mod tidy

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 )
 
 const completeExampleTerraformDir = "examples/complete"
