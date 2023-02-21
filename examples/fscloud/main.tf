d569ba0f7bfb7c997dab41e0875e684666e96382
Daniel Butler <dannyeb@gmail.com>
2023-02-21 14:11:41 +0000

    fix: private buckets
    Signed-off-by: Daniel Butler <dannyeb@gmail.com>

--- examples/fscloud/main.tf
+++ examples/fscloud/main.tf
@@ -122,16 +122,6 @@ module "cos_instance_and_bucket" {
       description      = "sample rule for bucket 1"
       enforcement_mode = "report"
       account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
-      tags = [
-        {
-          name  = "environment"
-          value = "${var.prefix}-test"
-        },
-        {
-          name  = "terraform-rule"
-          value = "allow-${var.prefix}-vpc-to-${var.prefix}-cos-${var.prefix}-bucket-1"
-        }
-      ]
       rule_contexts = [{
         attributes = [
           {
@@ -150,14 +140,11 @@ module "cos_instance_and_bucket" {
       description      = "sample rule for the instance"
       enforcement_mode = "report"
       account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
+      # IAM tags on the rule resources should match to the instance level IAM tags
       tags = [
         {
-          name  = "environment"
-          value = "${var.prefix}-test"
-        },
-        {
-          name  = "terraform-rule"
-          value = "allow-${var.prefix}-vpc-to-${var.prefix}-cos-${var.prefix}-instance"
+          name  = "env"
+          value = "test"
         }
       ]
       rule_contexts = [{
@@ -173,4 +160,11 @@ module "cos_instance_and_bucket" {
       }]
     }
   ]
+}
+
+# IAM tags for the instance to match to the CBR rule tags.
+resource "ibm_resource_tag" "instance_tags" {
+  resource_id = module.cos_instance_and_bucket.cos_instance_id
+  tag_type    = "access" # MUST be type access for CBR rule to work
+  tags        = ["env:test"]
 }
--- main.tf
+++ main.tf
@@ -79,6 +79,7 @@ resource "ibm_cos_bucket" "cos_bucket" {
   bucket_name           = var.bucket_name
   resource_instance_id  = local.cos_instance_id
   region_location       = var.region
+  endpoint_type         = var.bucket_endpoint
   cross_region_location = var.cross_region_location
   endpoint_type         = var.bucket_endpoint
   storage_class         = var.bucket_storage_class
