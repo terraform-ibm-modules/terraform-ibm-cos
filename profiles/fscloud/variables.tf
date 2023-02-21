8bed2445f320aced1c6496af54ca5097a3959c87
Daniel Butler <dannyeb@gmail.com>
2023-02-21 14:11:41 +0000

    fix: private buckets
    Signed-off-by: Daniel Butler <dannyeb@gmail.com>

--- profiles/fscloud/variables.tf
+++ profiles/fscloud/variables.tf
@@ -184,7 +184,7 @@ variable "bucket_cbr_rules" {
     tags = optional(list(object({
       name  = string
       value = string
-    })))
+    })), [])
     operations = optional(list(object({
       api_types = list(object({
         api_type_id = string
@@ -209,7 +209,7 @@ variable "instance_cbr_rules" {
     tags = optional(list(object({
       name  = string
       value = string
-    })))
+    })), [])
     operations = optional(list(object({
       api_types = list(object({
         api_type_id = string
