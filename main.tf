c101aac232d9c3ff656bb4a4120c165949100a4b
shemau <shemau@gmail.com>
2023-02-27 15:03:02 +0000

    * fix: apply endpoints to both buckets
    * fix: apply endpoints to both buckets
    
    
    ---------
    
    Co-authored-by: Steve Peggs <peggs@uk.ibm.com>

--- main.tf
+++ main.tf
@@ -80,6 +80,7 @@ resource "ibm_cos_bucket" "cos_bucket" {
   resource_instance_id  = local.cos_instance_id
   region_location       = var.region
   cross_region_location = var.cross_region_location
+  endpoint_type         = var.bucket_endpoint
   storage_class         = var.bucket_storage_class
   key_protect           = var.key_protect_key_crn
   ## This for_each block is NOT a loop to attach to multiple retention blocks.
@@ -144,9 +145,9 @@ resource "ibm_cos_bucket" "cos_bucket" {
 
 # Create COS bucket with:
 # - Retention
-# - Encryption
 # - Monitoring
 # - Activity Tracking
+# - Versioning
 # Create COS bucket without:
 # - Encryption
 resource "ibm_cos_bucket" "cos_bucket1" {
