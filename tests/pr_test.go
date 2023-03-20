e5584fe37cc45ef932bec76e267b6ec10ef37d4b
Rajat Agrawal <rajat.agrawal@ibm.com>
2023-03-20 13:58:03 +0000

    feat: fscloud

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -88,7 +88,6 @@ func TestRunFSCloudExample(t *testing.T) {
 	t.Parallel()
 
 	options := setupOptions(t, "cos-fscloud", fsCloudTerraformDir)
-	options.TerraformVars["bucket_endpoint"] = "private"
 	output, err := options.RunTestConsistency()
 	assert.Nil(t, err, "This should not have errored")
 	assert.NotNil(t, output, "Expected some output")
