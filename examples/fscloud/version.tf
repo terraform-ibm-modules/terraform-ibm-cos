6983b5097d798d246ba8e11ebb8004c513c34be3
Daniel Butler <dannyeb@gmail.com>
2023-03-30 10:13:16 +0100

    test: pin example provider version
    Signed-off-by: Daniel Butler <dannyeb@gmail.com>

--- examples/fscloud/version.tf
+++ examples/fscloud/version.tf
@@ -1,9 +1,10 @@
 terraform {
   required_version = ">= 1.0.0"
   required_providers {
+    # Pin to the lowest provider version of the range defined in the main module to ensure lowest version still works
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = ">= 1.51.0"
+      version = "1.51.0"
     }
     # The logdna provider is not actually required by the module itself, just this example, so OK to use ">=" here instead of locking into a version
     logdna = {
