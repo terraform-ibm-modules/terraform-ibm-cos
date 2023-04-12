c13de8756ed1a0a2cb921f9924d5847b5bd2e314
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2023-04-12 15:31:10 +0100

    feat: add one-rate plan

--- examples/one-rate-plan/main.tf
+++ examples/one-rate-plan/main.tf
@@ -3,79 +3,10 @@
 ##############################################################################
 
 module "resource_group" {
-  source = "git::https://github.com/terraform-ibm-modules/terraform-ibm-resource-group.git?ref=v1.0.5"
+  source                       = "git::https://github.com/terraform-ibm-modules/terraform-ibm-resource-group.git?ref=v1.0.5"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
-}
-
-##############################################################################
-# VPC
-##############################################################################
-resource "ibm_is_vpc" "example_vpc" {
-  name           = "${var.prefix}-vpc"
-  resource_group = module.resource_group.resource_group_id
-  tags           = var.resource_tags
-}
-
-resource "ibm_is_subnet" "testacc_subnet" {
-  name                     = "${var.prefix}-subnet"
-  vpc                      = ibm_is_vpc.example_vpc.id
-  zone                     = "${var.region}-1"
-  total_ipv4_address_count = 256
-  resource_group           = module.resource_group.resource_group_id
-}
-
-##############################################################################
-# Observability Instances (Sysdig + AT)
-##############################################################################
-
-locals {
-  existing_at = var.existing_at_instance_crn != null ? true : false
-  at_crn      = var.existing_at_instance_crn == null ? module.observability_instances.activity_tracker_crn : var.existing_at_instance_crn
-}
-
-# Create Sysdig and Activity Tracker instance
-module "observability_instances" {
-  source = "git::https://github.com/terraform-ibm-modules/terraform-ibm-observability-instances?ref=v2.5.1"
-  providers = {
-    logdna.at = logdna.at
-    logdna.ld = logdna.ld
-  }
-  region                         = var.region
-  resource_group_id              = module.resource_group.resource_group_id
-  sysdig_instance_name           = "${var.prefix}-sysdig"
-  sysdig_plan                    = "graduated-tier"
-  enable_platform_logs           = false
-  enable_platform_metrics        = false
-  logdna_provision               = false
-  activity_tracker_instance_name = "${var.prefix}-at"
-  activity_tracker_tags          = var.resource_tags
-  activity_tracker_plan          = "7-day"
-  activity_tracker_provision     = !local.existing_at
-  logdna_tags                    = var.resource_tags
-  sysdig_tags                    = var.resource_tags
-}
-
-##############################################################################
-# Create Key Protect resources
-##############################################################################
-
-locals {
-  key_ring_name = "cos-key-ring"
-  key_name      = "cos-key"
-}
-
-module "key_protect_all_inclusive" {
-  source                    = "git::https://github.com/terraform-ibm-modules/terraform-ibm-key-protect-all-inclusive.git?ref=v4.0.0"
-  key_protect_instance_name = "${var.prefix}-kp"
-  resource_group_id         = module.resource_group.resource_group_id
-  enable_metrics            = false
-  region                    = var.region
-  key_map = {
-    (local.key_ring_name) = [local.key_name]
-  }
-  resource_tags = var.resource_tags
 }
 
 ##############################################################################
@@ -88,17 +19,6 @@ data "ibm_iam_account_settings" "iam_account_settings" {
 ##############################################################################
 # Create CBR Zone
 ##############################################################################
-module "cbr_zone" {
-  source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-cbr//cbr-zone-module?ref=v1.2.0"
-  name             = "${var.prefix}-VPC-network-zone"
-  zone_description = "CBR Network zone containing VPC"
-  account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
-  addresses = [{
-    type  = "vpc", # to bind a specific vpc to the zone
-    value = ibm_is_vpc.example_vpc.crn,
-  }]
-}
-
 # Create COS instance and Key protect instance.
 # Create COS bucket-1 with:
 # - Encryption
@@ -106,37 +26,33 @@ module "cbr_zone" {
 # - Activity Tracking
 # - One Rate Plan & One Rate Active Bucket Storage Class
 module "cos_bucket" {
-  source                     = "../../"
-  resource_group_id          = module.resource_group.resource_group_id
-  region                     = var.region
-  cross_region_location      = null
-  cos_instance_name          = "${var.prefix}-cos"
-  cos_tags                   = var.resource_tags
-  bucket_name                = "${var.prefix}-bucket-1"
-  existing_kms_instance_guid = module.key_protect_all_inclusive.key_protect_guid
-  kms_key_crn                = module.key_protect_all_inclusive.keys["${local.key_ring_name}.${local.key_name}"].crn
-  sysdig_crn                 = module.observability_instances.sysdig_crn
+  source                = "../../"
+  resource_group_id     = module.resource_group.resource_group_id
+  region                = var.region
+  cross_region_location = null
+  cos_instance_name     = "${var.prefix}-cos"
+  cos_tags              = var.resource_tags
+  bucket_name           = "${var.prefix}-bucket-1"
   # disable retention for test environments - enable for stage/prod
-  retention_enabled    = false
-  cos_plan             = "cos-one-rate-plan"
-  bucket_storage_class = "onerate_active"
-  activity_tracker_crn = local.at_crn
-  bucket_cbr_rules = [
+  retention_enabled     = false
+  encryption_enabled    = false
+  cos_plan              = "cos-one-rate-plan"
+  bucket_storage_class  = "onerate_active"
+  bucket_cbr_rules      = [
     {
       description      = "sample rule for bucket 1"
       enforcement_mode = "report"
       account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
-      rule_contexts = [{
-        attributes = [
-          {
-            "name" : "endpointType",
-            "value" : "private"
-          },
-          {
-            name  = "networkZoneId"
-            value = module.cbr_zone.zone_id
-        }]
-      }]
+      rule_contexts    = [
+        {
+          attributes = [
+            {
+              "name" : "endpointType",
+              "value" : "private"
+            },
+          ]
+        }
+      ]
     }
   ]
   instance_cbr_rules = [
@@ -145,23 +61,22 @@ module "cos_bucket" {
       enforcement_mode = "report"
       account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
       # IAM tags on the rule resources should match to the instance level IAM tags
-      tags = [
+      tags             = [
         {
           name  = "env"
           value = "test"
         }
       ]
-      rule_contexts = [{
-        attributes = [
-          {
-            "name" : "endpointType",
-            "value" : "private"
-          },
-          {
-            name  = "networkZoneId"
-            value = module.cbr_zone.zone_id
-        }]
-      }]
+      rule_contexts = [
+        {
+          attributes = [
+            {
+              "name" : "endpointType",
+              "value" : "private"
+            },
+          ]
+        }
+      ]
     }
   ]
 }
