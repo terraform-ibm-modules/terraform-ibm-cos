168c39dc4c80dc45c3c789deb783b85b289d9d8f
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2023-04-12 15:47:21 +0100

    feat: add one-rate plan

--- examples/one-rate-plan/main.tf
+++ examples/one-rate-plan/main.tf
@@ -3,7 +3,7 @@
 ##############################################################################
 
 module "resource_group" {
-  source                       = "git::https://github.com/terraform-ibm-modules/terraform-ibm-resource-group.git?ref=v1.0.5"
+  source = "git::https://github.com/terraform-ibm-modules/terraform-ibm-resource-group.git?ref=v1.0.5"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -34,16 +34,16 @@ module "cos_bucket" {
   cos_tags              = var.resource_tags
   bucket_name           = "${var.prefix}-bucket-1"
   # disable retention for test environments - enable for stage/prod
-  retention_enabled     = false
-  encryption_enabled    = false
-  cos_plan              = "cos-one-rate-plan"
-  bucket_storage_class  = "onerate_active"
-  bucket_cbr_rules      = [
+  retention_enabled    = false
+  encryption_enabled   = false
+  cos_plan             = "cos-one-rate-plan"
+  bucket_storage_class = "onerate_active"
+  bucket_cbr_rules = [
     {
       description      = "sample rule for bucket 1"
       enforcement_mode = "report"
       account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
-      rule_contexts    = [
+      rule_contexts = [
         {
           attributes = [
             {
@@ -61,7 +61,7 @@ module "cos_bucket" {
       enforcement_mode = "report"
       account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
       # IAM tags on the rule resources should match to the instance level IAM tags
-      tags             = [
+      tags = [
         {
           name  = "env"
           value = "test"
