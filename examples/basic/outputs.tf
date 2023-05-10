bdc967b6ac2a885c9fe6284f5fa221dc1f073e18
Rajat Agrawal <rajat.agrawal@ibm.com>
2023-05-10 13:07:03 +0100

    feat: added basic example

--- examples/basic/outputs.tf
+++ examples/basic/outputs.tf
@@ -2,3 +2,13 @@ output "bucket_name" {
   description = "Bucket Name"
   value       = module.cos_bucket.bucket_name
 }
+
+output "bucket_crn" {
+  description = "Bucket CRN"
+  value       = module.cos_bucket.bucket_crn
+}
+
+output "bucket_id" {
+  description = "Bucket id"
+  value       = module.cos_bucket.bucket_id
+}
