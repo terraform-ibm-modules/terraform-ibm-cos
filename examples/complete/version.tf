7efbcbd85be5b7b7c474cc6957e5b3190408033a
Matthew Lemmond <Matthew.Lemmond@ibm.com>
2023-06-24 04:53:52 -0400

    feat: updated required ibm provider version constraint to `>= 1.54.0, < 2.0.0` to pickup this [fix](https://github.com/IBM-Cloud/terraform-provider-ibm/issues/4413) (#420)

--- examples/complete/version.tf
+++ examples/complete/version.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = ">= 1.0.0"
   required_providers {
-    # Pin to the lowest provider version of the range defined in the main module to ensure lowest version still works
+    # Use latest version of provider in non-basic examples to verify latest version works with module
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = ">= 1.51.0"
+      version = ">= 1.54.0"
     }
     # The restapi provider is not actually required by the module itself, just this example, so OK to use ">=" here instead of locking into a version
     restapi = {
