ecc6ca67caf3ea965490280e9bd87b7b8a2175e8
akocbek <106765658+akocbek@users.noreply.github.com>
2023-08-24 08:19:57 +0100

    feat: update required ibm provider version to >= 1.56.1 (#428)
    Co-authored-by: akocbek@ie.ibm.com <akocbek@ie.ibm.com>

--- examples/fscloud/version.tf
+++ examples/fscloud/version.tf
@@ -4,7 +4,7 @@ terraform {
     # Use latest version of provider in non-basic examples to verify latest version works with module
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = ">= 1.54.0"
+      version = ">= 1.56.1"
     }
     # The logdna provider is not actually required by the module itself, just this example, so OK to use ">=" here instead of locking into a version
     logdna = {
