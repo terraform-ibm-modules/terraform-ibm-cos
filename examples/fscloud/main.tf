12434d102a658dd10b3bc945974854d555f731a1
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2023-09-27 10:50:23 +0000

    fix(deps): update terraform-module

--- examples/fscloud/main.tf
+++ examples/fscloud/main.tf
@@ -40,7 +40,7 @@ locals {
 # Create Sysdig and Activity Tracker instance
 module "observability_instances" {
   source  = "terraform-ibm-modules/observability-instances/ibm"
-  version = "2.9.1"
+  version = "2.9.2"
   providers = {
     logdna.at = logdna.at
     logdna.ld = logdna.ld
@@ -73,7 +73,7 @@ data "ibm_iam_account_settings" "iam_account_settings" {
 
 module "cbr_zone" {
   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-zone-module"
-  version          = "1.9.0"
+  version          = "1.12.1"
   name             = "${var.prefix}-VPC-network-zone"
   zone_description = "CBR Network zone containing VPC"
   account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
