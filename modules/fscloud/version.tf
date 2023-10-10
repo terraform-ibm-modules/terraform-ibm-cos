de7ddf844be06bf859a5843d2a82b65a12718122
akocbek <106765658+akocbek@users.noreply.github.com>
2023-10-10 17:04:29 +0100

    feat: update terraform version constraints to `>= 1.3.0, <1.6.0` (#447)
    Co-authored-by: Andrej <akocbek@ie.ibm.com>

--- modules/fscloud/version.tf
+++ modules/fscloud/version.tf
@@ -2,7 +2,7 @@
 # Terraform Version
 ##############################################################################
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.3.0, <1.6.0"
 
   # Use a flexible range in modules that future proofs the module's usage with upcoming minor and patch versions
   required_providers {
