c78aed31c5685cd3381394e9cd84552872e95a38
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2023-12-14 13:11:22 +0000

    fix(deps): update terraform terraform-ibm-modules/cbr/ibm to v1.17.1

--- examples/fscloud/main.tf
+++ examples/fscloud/main.tf
@@ -73,7 +73,7 @@ data "ibm_iam_account_settings" "iam_account_settings" {
 
 module "cbr_zone" {
   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-zone-module"
-  version          = "1.17.0"
+  version          = "1.17.1"
   name             = "${var.prefix}-VPC-fscloud-nz"
   zone_description = "CBR Network zone containing VPC"
   account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
