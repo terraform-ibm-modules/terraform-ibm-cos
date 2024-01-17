462215818d9f704374056720151e9f5a8c66d5bf
akocbek <106765658+akocbek@users.noreply.github.com>
2024-01-17 10:24:05 +0000

    fix: made `resource_group_id` optional, as its only required when create a COs instance (not required for bucket creation). `resource_group_id` has also been removed as an option in the `bucket_configs` variable for the buckets submodule. (#513)

--- modules/fscloud/variables.tf
+++ modules/fscloud/variables.tf
@@ -78,7 +78,6 @@ variable "bucket_configs" {
     cross_region_location         = optional(string, null)
     storage_class                 = optional(string, "smart")
     region_location               = optional(string, null)
-    resource_group_id             = string
     resource_instance_id          = optional(string, null)
 
     activity_tracking = optional(object({
