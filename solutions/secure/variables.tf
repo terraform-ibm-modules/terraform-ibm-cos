ea5802b1aba8b418be7cb922d93039d83bd7902a
dishankkalra23 <dishankalra@gmail.com>
2024-02-22 16:58:43 +0530

    fix: testing da

--- solutions/secure/variables.tf
+++ solutions/secure/variables.tf
@@ -83,102 +83,109 @@ variable "access_tags" {
 ##############################################################################
 # COS bucket variables
 ##############################################################################
-variable "bucket_configs" {
-  type = list(object({
-    access_tags                   = optional(list(string), [])
-    bucket_name                   = string
-    kms_encryption_enabled        = optional(bool, true)
-    kms_guid                      = optional(string, null)
-    kms_key_crn                   = string
-    skip_iam_authorization_policy = optional(bool, false)
-    management_endpoint_type      = string
-    cross_region_location         = optional(string, null)
-    storage_class                 = optional(string, "smart")
-    region_location               = optional(string, null)
-    resource_instance_id          = optional(string, null)
-    force_delete                  = optional(bool, true)
-    single_site_location          = optional(string, null)
-    hard_quota                    = optional(number, null)
+# variable "bucket_configs" {
+#   type = list(object({
+#     access_tags                   = optional(list(string), [])
+#     bucket_name                   = string
+#     kms_encryption_enabled        = optional(bool, true)
+#     kms_guid                      = optional(string, null)
+#     kms_key_crn                   = string
+#     skip_iam_authorization_policy = optional(bool, false)
+#     management_endpoint_type      = string
+#     cross_region_location         = optional(string, null)
+#     storage_class                 = optional(string, "smart")
+#     region_location               = optional(string, null)
+#     resource_instance_id          = optional(string, null)
+#     force_delete                  = optional(bool, true)
+#     single_site_location          = optional(string, null)
+#     hard_quota                    = optional(number, null)
 
-    activity_tracking = optional(object({
-      read_data_events     = optional(bool, true)
-      write_data_events    = optional(bool, true)
-      activity_tracker_crn = optional(string, null)
-    }))
-    archive_rule = optional(object({
-      enable = optional(bool, false)
-      days   = optional(number, 20)
-      type   = optional(string, "Glacier")
-    }))
-    expire_rule = optional(object({
-      enable = optional(bool, false)
-      days   = optional(number, 365)
-    }))
-    metrics_monitoring = optional(object({
-      usage_metrics_enabled   = optional(bool, true)
-      request_metrics_enabled = optional(bool, true)
-      metrics_monitoring_crn  = optional(string, null)
-    }))
-    object_versioning = optional(object({
-      enable = optional(bool, false)
-    }))
-    retention_rule = optional(object({
-      default   = optional(number, 90)
-      maximum   = optional(number, 350)
-      minimum   = optional(number, 90)
-      permanent = optional(bool, false)
-    }))
-    cbr_rules = optional(list(object({
-      description = string
-      account_id  = string
-      rule_contexts = list(object({
-        attributes = optional(list(object({
-          name  = string
-          value = string
-      }))) }))
-      enforcement_mode = string
-      tags = optional(list(object({
-        name  = string
-        value = string
-      })), [])
-      operations = optional(list(object({
-        api_types = list(object({
-          api_type_id = string
-        }))
-      })))
-    })), [])
+#     activity_tracking = optional(object({
+#       read_data_events     = optional(bool, true)
+#       write_data_events    = optional(bool, true)
+#       activity_tracker_crn = optional(string, null)
+#     }))
+#     archive_rule = optional(object({
+#       enable = optional(bool, false)
+#       days   = optional(number, 20)
+#       type   = optional(string, "Glacier")
+#     }))
+#     expire_rule = optional(object({
+#       enable = optional(bool, false)
+#       days   = optional(number, 365)
+#     }))
+#     metrics_monitoring = optional(object({
+#       usage_metrics_enabled   = optional(bool, true)
+#       request_metrics_enabled = optional(bool, true)
+#       metrics_monitoring_crn  = optional(string, null)
+#     }))
+#     object_versioning = optional(object({
+#       enable = optional(bool, false)
+#     }))
+#     retention_rule = optional(object({
+#       default   = optional(number, 90)
+#       maximum   = optional(number, 350)
+#       minimum   = optional(number, 90)
+#       permanent = optional(bool, false)
+#     }))
+#     cbr_rules = optional(list(object({
+#       description = string
+#       account_id  = string
+#       rule_contexts = list(object({
+#         attributes = optional(list(object({
+#           name  = string
+#           value = string
+#       }))) }))
+#       enforcement_mode = string
+#       tags = optional(list(object({
+#         name  = string
+#         value = string
+#       })), [])
+#       operations = optional(list(object({
+#         api_types = list(object({
+#           api_type_id = string
+#         }))
+#       })))
+#     })), [])
 
-  }))
-  description = "Cloud Object Storage bucket configurations"
-  default     = []
-  # Validation happens in the fscloud module
-}
+#   }))
+#   description = "Cloud Object Storage bucket configurations"
+#   default     = []
+#   # Validation happens in the fscloud module
+# }
 
 ##############################################################
 # Context-based restriction (CBR)
 ##############################################################
 
-variable "instance_cbr_rules" {
-  type = list(object({
-    description = string
-    account_id  = string
-    rule_contexts = list(object({
-      attributes = optional(list(object({
-        name  = string
-        value = string
-    }))) }))
-    enforcement_mode = string
-    tags = optional(list(object({
-      name  = string
-      value = string
-    })), [])
-    operations = optional(list(object({
-      api_types = list(object({
-        api_type_id = string
-      }))
-    })))
-  }))
-  description = "(Optional, list) List of CBR rule to create for the instance"
-  default     = []
-  # Validation happens in the rule module
+# variable "instance_cbr_rules" {
+#   type = list(object({
+#     description = string
+#     account_id  = string
+#     rule_contexts = list(object({
+#       attributes = optional(list(object({
+#         name  = string
+#         value = string
+#     }))) }))
+#     enforcement_mode = string
+#     tags = optional(list(object({
+#       name  = string
+#       value = string
+#     })), [])
+#     operations = optional(list(object({
+#       api_types = list(object({
+#         api_type_id = string
+#       }))
+#     })))
+#   }))
+#   description = "(Optional, list) List of CBR rule to create for the instance"
+#   default     = []
+#   # Validation happens in the rule module
+# }
+
+
+variable "prefix" {
+  type        = string
+  description = "Prefix name for all related resources"
+  default     = "da-dishank"
 }
