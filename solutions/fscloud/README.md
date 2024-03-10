43cf9719ef8c0f692f5fcb9b805357a723af9066
dishankkalra23 <dishankalra@gmail.com>
2024-03-10 22:01:04 +0530

    fix: update service name

--- solutions/fscloud/README.md
+++ solutions/fscloud/README.md
@@ -1,9 +1,9 @@
-# Cloud Object Storage for IBM Cloud - Financial Services Cloud solution
+# Cloud-Object-Storage for IBM Cloud - Financial Services Cloud solution
 
-This architecture creates an Cloud Object Storage(COS) and bucket on IBM Cloud® and supports provisioning the following resources:
+This architecture creates an Cloud-Object-Storage and bucket on IBM Cloud® and supports provisioning the following resources:
 
 - A resource group, if one is not passed in.
-- A Cloud Object Storage(COS) for IBM Cloud, set up with
+- A Cloud-Object-Storage for IBM Cloud, set up with
     - KMS encryption to encrypt data
     - Monitoring and Activity Tracking to capture information regarding the events in the COS.
 - Context Based Restriction rules for the instance and buckets
--- tests/pr_test.go
+++ tests/pr_test.go
@@ -33,7 +33,7 @@ const fsCloudTerraformDir = "examples/fscloud"
 const replicateExampleTerraformDir = "examples/replication"
 const basicExampleTerraformDir = "examples/basic"
 const oneRateExampleTerraformDir = "examples/one-rate-plan"
-const solutionSecure = "solutions/secure"
+const solutionFscloudDir = "solutions/fscloud"
 
 // Use existing group for tests
 const resourceGroup = "geretain-test-cos-base"
@@ -83,7 +83,7 @@ func setupOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptio
 			},
 		})
 	}
-	if dir == solutionSecure {
+	if dir == solutionFscloudDir {
 		options = testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
 			Testing:      t,
 			TerraformDir: dir,
@@ -270,10 +270,10 @@ func getCOSInstanceClient(apiKey, serviceInstanceID, authEndpoint, serviceEndpoi
 	return s3.New(sess, conf)
 }
 
-func TestRunSecureSolution(t *testing.T) {
+func TestRunFsCloudSolution(t *testing.T) {
 	t.Parallel()
 
-	options := setupOptions(t, "cos-da", solutionSecure)
+	options := setupOptions(t, "cos-da", solutionFscloudDir)
 	options.TerraformVars = map[string]interface{}{
 		"existing_resource_group": true,
 		"cos_instance_name":       "cos-da",
