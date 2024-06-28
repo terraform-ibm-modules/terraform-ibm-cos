876f5c83d5798f65ceb6b812e825789568080bb9
huayuenh <huayuenh@ie.ibm.com>
2024-06-28 13:14:30 +0100

    feat: add individual cos bucket detail outputs

--- solutions/secure-regional-bucket/outputs.tf
+++ solutions/secure-regional-bucket/outputs.tf
@@ -5,3 +5,23 @@ output "buckets" {
   description = "List of buckets created"
   value       = module.cos.buckets
 }
+
+output "s3_endpoint_direct" {
+  description = "The s3 direct endpoint of the created bucket."
+  value       = try(module.cos.buckets[var.bucket_name].s3_endpoint_direct, "")
+}
+
+output "s3_endpoint_private" {
+  description = "The s3 private endpoint of the created bucket."
+  value       = try(module.cos.buckets[var.bucket_name].s3_endpoint_private, "")
+}
+
+output "bucket_name" {
+  description = "The name of the bucket that was created. Includes the optional suffix if enabled."
+  value       = try(module.cos.buckets[var.bucket_name].bucket_name, "")
+}
+
+output "cos_instance_crn" {
+  description = "The CRN of the COS instance containing the created bucket."
+  value       = var.existing_cos_instance_id
+}
