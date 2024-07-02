1a86818f643529fd104b935ba4a3a7444c098f65
ocofaigh <ocofaigh@ie.ibm.com>
2024-07-02 16:06:41 +0100

    version update

--- version.tf
+++ version.tf
@@ -4,8 +4,9 @@ terraform {
   # Use a flexible range in modules that future proofs the module's usage with upcoming minor and patch versions
   required_providers {
     ibm = {
-      source  = "ibm-cloud/ibm"
-      version = ">= 1.67.0-beta1, < 2.0.0"
+      source = "ibm-cloud/ibm"
+      # version = ">= 1.67.0-beta1, < 2.0.0"
+      version = ">= 1.67.0-beta1"
     }
     time = {
       source  = "hashicorp/time"
