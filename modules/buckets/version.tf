0eb73f2c6e694dfe54225857500810c0b8f0f6e5
ocofaigh <ocofaigh@ie.ibm.com>
2024-07-02 16:20:55 +0100

    versions

--- modules/buckets/version.tf
+++ modules/buckets/version.tf
@@ -5,8 +5,9 @@ terraform {
   required_providers {
     # tflint-ignore: terraform_unused_required_providers
     ibm = {
-      source  = "ibm-cloud/ibm"
-      version = ">= 1.67.0-beta1, < 2.0.0"
+      source = "ibm-cloud/ibm"
+      # version = ">= 1.67.0-beta1, < 2.0.0"
+      version = ">= 1.67.0-beta1"
     }
     time = {
       source  = "hashicorp/time"
--- modules/fscloud/README.md
+++ modules/fscloud/README.md
@@ -89,7 +89,7 @@ module "cos_fscloud" {
 | Name | Version |
 |------|---------|
 | <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.4.0 |
-| <a name="requirement_ibm"></a> [ibm](#requirement\_ibm) | >= 1.67.0-beta1, <2.0.0 |
+| <a name="requirement_ibm"></a> [ibm](#requirement\_ibm) | >= 1.67.0-beta1 |
 
 ### Modules
 
