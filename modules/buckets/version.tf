0843271524ff780e72b3ba2abb1de2fa67c7dfcd
ocofaigh <ocofaigh@ie.ibm.com>
2024-07-03 11:34:24 +0100

    fix version is submodules

--- modules/buckets/version.tf
+++ modules/buckets/version.tf
@@ -5,9 +5,8 @@ terraform {
   required_providers {
     # tflint-ignore: terraform_unused_required_providers
     ibm = {
-      source = "ibm-cloud/ibm"
-      # version = ">= 1.67.0-beta1, < 2.0.0"
-      version = ">= 1.66.0"
+      source  = "ibm-cloud/ibm"
+      version = ">= 1.67.0, < 2.0.0"
     }
     time = {
       source  = "hashicorp/time"
--- modules/fscloud/version.tf
+++ modules/fscloud/version.tf
@@ -6,9 +6,8 @@ terraform {
   required_providers {
     # tflint-ignore: terraform_unused_required_providers
     ibm = {
-      source = "ibm-cloud/ibm"
-      # version = ">= 1.67.0-beta1, <2.0.0"
-      version = ">= 1.67.0-beta1"
+      source  = "ibm-cloud/ibm"
+      version = ">= 1.67.0, <2.0.0"
     }
   }
 }
