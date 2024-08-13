48176ed77f6fd2a321fee5ed55104a569582e66e
Soaib024 <Soaib024@gmail.com>
2024-08-13 15:47:00 +0530

    address review comments

--- solutions/instance/main.tf
+++ solutions/instance/main.tf
@@ -60,7 +60,7 @@ locals {
   existing_secrets_manager_instance_region    = var.existing_secrets_manager_instance_crn != null ? element(local.existing_secrets_manager_instance_crn_split, length(local.existing_secrets_manager_instance_crn_split) - 5) : null
 
   # tflint-ignore: terraform_unused_declarations
-  validate_sm_crn = length(local.service_credential_secrets) > 0 && var.existing_secrets_manager_instance_crn == null ? tobool("`var.existing_secrets_manager_instance_crn` is required when adding service credentials to a secrets manager secret.") : false
+  validate_sm_crn = length(local.service_credential_secrets) > 0 && var.existing_secrets_manager_instance_crn == null ? tobool("`existing_secrets_manager_instance_crn` is required when adding service credentials to a secrets manager secret.") : false
 }
 
 module "secrets_manager_service_credentials" {
