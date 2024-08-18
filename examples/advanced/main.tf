fc18e9e04ee0ca99355c11a4a43e92223e819e2a
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-08-18 11:35:59 +0000

    fix(deps): update terraform-module

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -51,7 +51,7 @@ locals {
 # Create Monitoring and Activity Tracker instance
 module "observability_instances" {
   source  = "terraform-ibm-modules/observability-instances/ibm"
-  version = "2.13.2"
+  version = "2.14.0"
   providers = {
     logdna.at = logdna.at
     logdna.ld = logdna.ld
@@ -82,7 +82,7 @@ locals {
 
 module "key_protect_all_inclusive" {
   source                    = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                   = "4.15.2"
+  version                   = "4.15.8"
   key_protect_instance_name = "${var.prefix}-kp"
   resource_group_id         = module.resource_group.resource_group_id
   enable_metrics            = false
@@ -113,7 +113,7 @@ data "ibm_iam_account_settings" "iam_account_settings" {
 
 module "cbr_zone" {
   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-zone-module"
-  version          = "1.23.0"
+  version          = "1.24.0"
   name             = "${var.prefix}-VPC-network-zone"
   zone_description = "CBR Network zone containing VPC"
   account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
