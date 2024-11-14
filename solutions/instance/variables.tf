3583b31d19f2fa5c1f2bcbeda3daee39eb6fb519
kierra.searle@ibm.com <kierra.searle@ibm.com>
2024-11-14 13:56:26 -0500

    refactor: added additional prefix validation

--- solutions/instance/variables.tf
+++ solutions/instance/variables.tf
@@ -8,6 +8,10 @@ variable "prefix" {
   type        = string
   description = "(Optional) Prefix to append to all resources created by this solution."
   default     = null
+  validation {
+    error_message = "Prefix must begin with a lowercase letter and contain only lowercase letters, numbers, and - characters. Prefixes must end with a lowercase letter or number and be 16 or fewer characters."
+    condition     = can(regex("^([a-z]|[a-z][-a-z0-9]*[a-z0-9])$", var.prefix)) && length(var.prefix) <= 16
+  }
 }
 
 variable "existing_resource_group" {
