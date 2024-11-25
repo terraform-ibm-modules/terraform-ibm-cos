e3b00a951b21c23f649731e192567e96989fac01
Md Anam Raihan <md.anam.raihan17@gmail.com>
2024-11-25 07:15:19 +0000

    fix: add resource group name output

--- solutions/instance/outputs.tf
+++ solutions/instance/outputs.tf
@@ -6,6 +6,11 @@ output "resource_group_id" {
   value       = module.resource_group.resource_group_id
 }
 
+output "resource_group_name" {
+  description = "Resource group name"
+  value       = module.resource_group.resource_group_name
+}
+
 output "cos_instance_id" {
   description = "COS instance id"
   value       = module.cos.cos_instance_id
