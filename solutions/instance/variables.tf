0391a99dc1a3ef85d1757f2ad2b89224583d71dd
Arya Girish K <arya.girish.k@ibm.com>
2025-01-10 13:37:09 +0530

    refactor:Update the prefix logic

--- solutions/instance/variables.tf
+++ solutions/instance/variables.tf
@@ -6,8 +6,8 @@ variable "ibmcloud_api_key" {
 
 variable "prefix" {
   type        = string
-  description = "(Optional) Prefix to append to all resources created by this solution."
-  default     = null
+  description = "(Optional) Prefix to append to all resources created by this solution. Prefix value can be an empty string (\"\") or `null` for advanced users."
+  default     = "cosda"
 }
 
 variable "existing_resource_group" {
