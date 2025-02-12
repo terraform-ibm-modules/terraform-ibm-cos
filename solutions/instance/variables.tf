3f33a8e40a530e14d3e8029ee484263cf562ad55
kierra.searle@ibm.com <kierra.searle@ibm.com>
2025-02-12 13:38:25 -0500

    fix: catalog.json formatting and remove validation from instance da

--- solutions/instance/variables.tf
+++ solutions/instance/variables.tf
@@ -8,15 +8,6 @@ variable "prefix" {
   type        = string
   description = "(Optional) Prefix to add to all resources created by this solution. To not use any prefix value, you can set this value to `null` or an empty string."
   default     = "dev"
-  validation {
-    condition = (var.prefix == null ? true :
-      alltrue([
-        can(regex("^[a-z]{0,1}[-a-z0-9]{0,14}[a-z0-9]{0,1}$", var.prefix)),
-        length(regexall("^.*--.*", var.prefix)) == 0
-      ])
-    )
-    error_message = "Prefix must begin with a lowercase letter, contain only lowercase letters, numbers, and - characters. Prefixes must end with a lowercase letter or number and be 16 or fewer characters."
-  }
 }
 
 variable "existing_resource_group" {
