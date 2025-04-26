5360fa3645ff88602bba7b89c9514b4b3016bb4a
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-04-26 16:32:50 +0000

    fix(deps): update terraform-module

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -50,7 +50,7 @@ locals {
 
 module "key_protect_all_inclusive" {
   source                    = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                   = "5.0.0"
+  version                   = "5.0.1"
   key_protect_instance_name = "${var.prefix}-kp"
   resource_group_id         = module.resource_group.resource_group_id
   enable_metrics            = false
@@ -81,7 +81,7 @@ data "ibm_iam_account_settings" "iam_account_settings" {
 
 module "cbr_zone" {
   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-zone-module"
-  version          = "1.30.0"
+  version          = "1.31.0"
   name             = "${var.prefix}-VPC-network-zone"
   zone_description = "CBR Network zone containing VPC"
   account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
