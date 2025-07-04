34fa240d9d3ae61e4b81572b4dacf0ceee1b45c2
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-07-04 23:24:34 +0000

    fix(deps): update terraform ibm to latest for the deployable architecture solution

--- solutions/regional-bucket/fully-configurable/version.tf
+++ solutions/regional-bucket/fully-configurable/version.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.79.2"
+      version = "1.80.0"
     }
     time = {
       source  = "hashicorp/time"
