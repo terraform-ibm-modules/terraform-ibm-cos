40ca41f8274f7222b6c0c487eeaa88a7a8047409
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-07-19 12:13:01 +0100

    fix(deps): update terraform terraform-ibm-modules/cbr/ibm to v1.32.4 (#921)

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -81,7 +81,7 @@ data "ibm_iam_account_settings" "iam_account_settings" {
 
 module "cbr_zone" {
   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-zone-module"
-  version          = "1.32.3"
+  version          = "1.32.4"
   name             = "${var.prefix}-VPC-network-zone"
   zone_description = "CBR Network zone containing VPC"
   account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
