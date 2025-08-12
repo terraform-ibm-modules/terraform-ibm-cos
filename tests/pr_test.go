5f7a453ce2e1b13f311a099b34028716649400df
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2025-08-12 20:43:21 +0530

    auth_policy error

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -621,7 +621,6 @@ func TestRunRegionalFullyConfigurableUpgradeSchematics(t *testing.T) {
 		{Name: "existing_cos_instance_crn", Value: permanentResources["general_test_storage_cos_instance_crn"], DataType: "string"},
 		{Name: "existing_kms_instance_crn", Value: permanentResources["hpcs_south_crn"], DataType: "string"},
 		{Name: "kms_encryption_enabled", Value: true, DataType: "bool"},
-		{Name: "skip_cos_kms_iam_auth_policy", Value: true, DataType: "bool"},
 		{Name: "bucket_name", Value: "reg-bucket", DataType: "string"},
 	}
 
