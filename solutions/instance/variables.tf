6adeef81dd40e386dcc400c400d5b3411788b645
Piyush Kumar Sahu <Piyush.Kumar.Sahu@ibm.com>
2025-09-24 18:48:59 +0530

    reverted

--- solutions/instance/variables.tf
+++ solutions/instance/variables.tf
@@ -34,7 +34,7 @@ variable "prefix" {
 variable "existing_resource_group_name" {
   type        = string
   description = "The name of an existing resource group to provision the resources. If not provided the default resource group will be used."
-  default     = "test-piyush-rg"
+  default     = null
 }
 
 variable "instance_name" {
