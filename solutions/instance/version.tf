fc1c91df468559b89e8612838037793448eb8b54
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-11-10 01:02:46 +0000

    fix(deps): update dependencies (#993)

--- solutions/instance/version.tf
+++ solutions/instance/version.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.84.3"
+      version = "1.85.0"
     }
     time = {
       source  = "hashicorp/time"
