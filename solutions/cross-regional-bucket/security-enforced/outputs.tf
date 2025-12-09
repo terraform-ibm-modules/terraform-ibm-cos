596e740d29026581a007648160586bd9d56981db
ocofaigh <ocofaigh@ie.ibm.com>
2025-12-09 13:21:26 +0000

    fix typo

--- solutions/cross-regional-bucket/security-enforced/outputs.tf
+++ solutions/cross-regional-bucket/security-enforced/outputs.tf
@@ -24,12 +24,12 @@ output "bucket_name" {
 
 output "cos_instance_crn" {
   description = "The CRN of the COS instance containing the created bucket."
-  value       = module.cross_regional_bucket.bucket_name.cos_instance_crn
+  value       = module.cross_regional_bucket.cos_instance_crn
 }
 
 output "cos_instance_guid" {
   description = "The guid of the COS instance containing the created bucket."
-  value       = module.cross_regional_bucket.bucket_name.cos_instance_guid
+  value       = module.cross_regional_bucket.cos_instance_guid
 }
 
 ##############################################################################
