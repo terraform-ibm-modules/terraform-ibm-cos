4faa0cfb0c825c43acd2d6e779d80482bba6ef64
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2026-02-03 12:16:07 +0000

    Update provider.tf

--- solutions/regional-bucket/fully-configurable/provider.tf
+++ solutions/regional-bucket/fully-configurable/provider.tf
@@ -2,7 +2,7 @@ provider "ibm" {
   alias                 = "cos"
   ibmcloud_api_key      = var.ibmcloud_api_key
   visibility            = var.provider_visibility
-  private_endpoint_type = (var.provider_visibility == "private" && var.region == "ca-mon") ? "vpe" : null
+  private_endpoint_type = var.provider_visibility == "private" && (var.region == "ca-mon" || var.region == "in-che") ? "vpe" : null
 }
 
 provider "ibm" {
@@ -10,5 +10,5 @@ provider "ibm" {
   ibmcloud_api_key      = var.ibmcloud_kms_api_key != null ? var.ibmcloud_kms_api_key : var.ibmcloud_api_key
   region                = local.kms_region
   visibility            = var.provider_visibility
-  private_endpoint_type = (var.provider_visibility == "private" && var.region == "ca-mon") ? "vpe" : null
+  private_endpoint_type = var.provider_visibility == "private" && (var.region == "ca-mon" || var.region == "in-che") ? "vpe" : null
 }
