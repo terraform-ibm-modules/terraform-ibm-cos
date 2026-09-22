1d68c2a7d935b36b822b9e18ee407f5fd299bf26
Khuzaima-Shakeel <khuzaima05>
2026-09-22 12:54:27 +0530

    minor fix

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -790,9 +790,8 @@ func TestRunRegionalFullyConfigurableWithKMSSchematics(t *testing.T) {
 		{Name: "ibmcloud_api_key", Value: options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], DataType: "string", Secure: true},
 		{Name: "prefix", Value: options.Prefix, DataType: "string"},
 		{Name: "kms_encryption_enabled", Value: true, DataType: "bool"},
-		{Name: "existing_kms_key_crn", Value: permanentResources["kp_dedicated_us_south_root_key_crn"], DataType: "string"},
+		{Name: "existing_kms_instance_crn", Value: permanentResources["kp_dedicated_us_south_crn"], DataType: "string"},
 		{Name: "existing_cos_instance_crn", Value: permanentResources["general_test_storage_cos_instance_crn"], DataType: "string"},
-		{Name: "skip_cos_kms_iam_auth_policy", Value: true, DataType: "bool"},
 		{Name: "bucket_name", Value: "reg-fc-kms-bucket", DataType: "string"},
 	}
 
